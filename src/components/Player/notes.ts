b7d9638016512beb79847ea4871e7553cf00df20
Zyuzin Vitaly <zyuzin.vitaly@gmail.com>
2021-11-18 13:32:14 +0300

    Убирает мелодию в отдельынй модуль

---
+++ src/components/Player/notes.ts
@@ -0,0 +1,196 @@
+export const notes = [
+  [
+    {
+      instrument: 'piano',
+      note: 'F'
+    },
+    {
+      instrument: 'drum',
+      note: 'kick'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'E'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'D'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'C'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'G'
+    },
+    {
+      instrument: 'drum',
+      note: 'kick'
+    }
+  ],
+  [],
+  [
+    {
+      instrument: 'piano',
+      note: 'G'
+    }
+  ],
+  [],
+  [
+    {
+      instrument: 'piano',
+      note: 'F'
+    },
+    {
+      instrument: 'drum',
+      note: 'kick'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'E'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'D'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'C'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'G'
+    },
+    {
+      instrument: 'drum',
+      note: 'kick'
+    }
+  ],
+  [],
+  [
+    {
+      instrument: 'piano',
+      note: 'G'
+    }
+  ],
+  [],
+  [
+    {
+      instrument: 'piano',
+      note: 'D'
+    },
+    {
+      instrument: 'drum',
+      note: 'kick'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'F'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'F'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'D'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'C'
+    },
+    {
+      instrument: 'drum',
+      note: 'kick'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'A'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'A'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'C'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'D'
+    },
+    {
+      instrument: 'drum',
+      note: 'kick'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'G'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'G'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'D'
+    }
+  ],
+  [
+    {
+      instrument: 'piano',
+      note: 'C'
+    },
+    {
+      instrument: 'drum',
+      note: 'kick'
+    }
+  ],
+  [],
+  [
+    {
+      instrument: 'piano',
+      note: 'C'
+    }
+  ],
+  [],
+]
